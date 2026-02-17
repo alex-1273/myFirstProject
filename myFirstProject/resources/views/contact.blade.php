1652714ffd6be649ca6fb00fc22d6135d3b906ce
alex-1273 <graham-a63@ulster.ac.uk>
2026-02-17 11:53:59 +0000

    Added files to website

--- myFirstProject/resources/views/contact.blade.php
+++
@@ -1,6 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Contact Page
-    </x-slot:heading>
-    <p>This is the contact page.</p>
-</x-layout>
--- myFirstProject/resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        Home Page
-    </x-slot:heading>
-    <h1>{{ $greeting }}, from Home Page</h1>
-    <p>Welcome to my first project!</p>
-</x-layout>
