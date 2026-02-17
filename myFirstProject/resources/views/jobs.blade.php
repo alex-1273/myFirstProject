a56e33d070be04e9919026f54e6aefaf772e295d
alex-1273 <graham-a63@ulster.ac.uk>
2026-02-17 12:01:46 +0000

    Added project files

--- myFirstProject/resources/views/jobs.blade.php
+++ myFirstProject/resources/views/jobs.blade.php
@@ -2,15 +2,8 @@
     <x-slot:heading>
         Jobs Listing
     </x-slot:heading>
-
-    <ul>
-        @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="text-blue-500 hover:underline">
-                    <strong>{{ $job['title'] }}</strong>:
-                    Pays {{ $job['salary'] }} per year
-                </a>
-            </li>
-        @endforeach
-    </ul>
+    <p>Lots and lots of jobs.</p>
+    @foreach ($jobs as $job)
+        <li>{{ $job['title']}}: Pays {{ $job['salary']}} Per year</li>
+    @endforeach
 </x-layout>
