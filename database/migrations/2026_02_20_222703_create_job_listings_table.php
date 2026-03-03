3962dda12168cdb42011613f01bfd7377e815d94
alex-1273 <graham-a63@ulster.ac.uk>
2026-03-03 12:24:01 +0000

    COMMIT ALL

--- database/migrations/2026_02_20_222703_create_job_listings_table.php
+++ database/migrations/2026_02_20_222703_create_job_listings_table.php
@@ -9,16 +9,19 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('job_listings', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-            $table->string('salary');
-            $table->timestamps();
-        });
-    }
+public function up(): void
+{
+    Schema::create('job_listings', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->foreignId('employer_id')
+              ->constrained('employers')
+              ->cascadeOnDelete();
 
+        $table->string('salary')->nullable();
+        $table->timestamps();
+    });
+}
     /**
      * Reverse the migrations.
      */
